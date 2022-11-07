9dcc63641b86d4d8c8a88dc026402516409d9538
zi12i <110373482+zi12i@users.noreply.github.com>
2022-11-07 23:27:33 +0900

    button in input - in progress

--- community/src/components/home.vue
+++ community/src/components/home.vue
@@ -5,11 +5,19 @@
       <!-- 왼쪽 상자 -->
       <b-col cols="8">
         <!-- 검색창 -->
-        <div class="mt-5 ml-2 mr-2">
-          <v-text-field label="검색어를 입력하세요" single-line solo></v-text-field>
+        <div class="mt-5 ml-5 mr-2">
+          <div>
+            <b-input-group>
+              <b-input-group-prepend>
+                <button class="">Button</button>
+              </b-input-group-prepend>
+
+              <input type="text" class="searchBar" />
+            </b-input-group>
+          </div>
         </div>
         <!-- 베스트 -->
-        <div>토픽베스트</div>
+        <div class="bestTopics">토픽베스트</div>
         <!-- 기본 다른 게시판들 -->
         <div>
           <div>카테고리 1</div>
@@ -20,7 +28,7 @@
       </b-col>
       <!-- 오른쪽 상자 -->
       <b-col cols="4">
-        <div class="mt-5 mr-2 ml-2 popularPost">실시간 인기글</div>
+        <div class="mt-5 mr-5 ml-2 popularPost">실시간 인기글</div>
       </b-col>
     </b-row>
     <app-footer />
@@ -43,6 +51,16 @@ export default {
 .popularPost {
   width: 90%;
   height: 400px;
+  background-color: antiquewhite;
+}
+.searchBar {
+  width: 80%;
+  border: solid;
+  height: 55px;
+  border-radius: 30px;
+}
+.bestTopics {
+  width: 80%;
   background-color: antiquewhite;
 }
 </style>
