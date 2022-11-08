b2f9940feb87251a94fb8da5ce8f7e6f936b8ff1
zi12i <110373482+zi12i@users.noreply.github.com>
2022-11-08 20:53:26 +0900

    search bar

--- community/src/components/home.vue
+++ community/src/components/home.vue
@@ -3,16 +3,21 @@
     <app-nav />
     <b-row>
       <!-- 왼쪽 상자 -->
-      <b-col cols="8">
+      <b-col cols="1"> </b-col>
+      <b-col cols="7">
         <!-- 검색창 -->
         <div class="mt-5 ml-5 mr-2">
           <div>
             <b-input-group>
-              <b-input-group-prepend>
-                <button class="">Button</button>
-              </b-input-group-prepend>
-
-              <input type="text" class="searchBar" />
+              <v-text-field
+                :append-icon="marker ? 'mdi-magnify' : 'mdi-magnify'"
+                solo
+                rounded
+                label="검색하실 내용을 입력해 주세요!"
+                type="text"
+                @click:append="sendMessage"
+                @click:prepend="changeIcon"
+              ></v-text-field>
             </b-input-group>
           </div>
         </div>
@@ -27,9 +32,10 @@
         </div>
       </b-col>
       <!-- 오른쪽 상자 -->
-      <b-col cols="4">
-        <div class="mt-5 mr-5 ml-2 popularPost">실시간 인기글</div>
+      <b-col cols="3">
+        <div class="mt-5 ml-2 popularPost">실시간 인기글</div>
       </b-col>
+      <b-col cols="1"> </b-col>
     </b-row>
     <app-footer />
   </div>
@@ -44,6 +50,15 @@ export default {
   components: {
     'app-nav': Nav,
     'app-footer': Footer
+  },
+  toggleMarker() {
+    this.marker = !this.marker
+  },
+  sendMessage() {
+    this.clearMessage()
+  },
+  clearMessage() {
+    this.message = ''
   }
 }
 </script>
