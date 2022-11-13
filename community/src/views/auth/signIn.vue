c70e1b1ded188e0aa57e9fb8c1555ae5e168054d
zi12i <110373482+zi12i@users.noreply.github.com>
2022-11-13 13:46:43 +0900

    auth - signin basic layout

---
+++ community/src/views/auth/signIn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="authContainer">
+      <div class="authCard">
+        <div>
+          <!-- 여기는 로고 -->
+        </div>
+        <div>
+          <!-- 입력창, 로그인버튼 들어갈 곳 -->
+        </div>
+        <div>
+          <!-- 회원가입, 아디비번 찾기 버튼 -->
+        </div>
+        <div>
+          <!-- 뒤로가기버튼 -->
+        </div>
+        <div>
+          <!-- footer 파트 -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.authContainer {
+  display: flex;
+  justify-content: center;
+}
+.authCard {
+  height: 100%;
+  min-height: 800px;
+  width: 600px;
+  background-color: #eee;
+}
+</style>
