65beef762f4dcab279eba857680263145a9a0059
zi12i <110373482+zi12i@users.noreply.github.com>
2022-11-15 19:33:42 +0900

    few adjustment

--- community/src/views/auth/signUp.vue
+++ community/src/views/auth/signUp.vue
@@ -13,7 +13,7 @@
           <div class="buttonOrganizer">
             <!-- 회원가입, 아디비번 찾기 버튼 -->
             <router-link to="/" class="auth">메인으로 돌아가기</router-link> |
-            <router-link to="/signup" class="auth">회원가입</router-link> |
+            <router-link to="/signin" class="auth">뒤로 가기</router-link> |
             <router-link to="/find" class="auth">ID / PW 찾기</router-link>
           </div>
         </div>
