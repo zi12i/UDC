7724ce163d4e41ae624e9dadb73516ff5f516881
zi12i <110373482+zi12i@users.noreply.github.com>
2022-11-23 16:27:12 +0900

    layout

--- community/src/components/boardCard.vue
+++ community/src/components/boardCard.vue
@@ -13,14 +13,17 @@ export default {}
 
 <style scoped>
 .cardContainer {
-  width: 1500%;
+  width: 100%;
   height: 150px;
   background-color: antiquewhite;
   display: flex;
-  justify-items: center;
+  justify-content: center;
   align-items: center;
 }
 .card {
+  width: 1000px;
+  height: 150px;
+  padding-top: 30px;
   background-color: azure;
 }
 </style>
